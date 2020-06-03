8d3aed8139c72099156b19d06bea6ffea8e0ae39
Fabiano Fernandes da Silva <60864727+bnhfernandes@users.noreply.github.com>
2020-06-03 17:58:24 -0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using SondaWebBaseContracts.Security;
+using SondaWebNoteService.Helpers;
+using SondaWebNoteService.MigrationsDataDefault;
+using SondaWebNoteService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SondaWebNoteService
+{
+    /// <summary>
+    /// Classe de controle de Inicialização do Serviço
+    /// </summary>
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Método de inicialização
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+           
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            
+            DevExpress.Utils.AzureCompatibility.Enable = true;
+        }
+    }
+}
